33ac20244a69b6d21d539812ec8b2749a2f73727
dzay <dhananjaysm0712@gmail.com>
2022-10-02 19:43:14 +0530

    ci: added componenets

---
+++ components/cards/CardComponent.mocks.ts
@@ -0,0 +1,15 @@
+import { ICardComponent } from './CardComponent';
+
+const base: ICardComponent = {
+
+  tag: 'Felines',
+  title: `What's new in Cats`,
+  body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!',
+  author: 'Alex',
+  time: '2h ago',
+
+};
+
+export const mockCardComponentProps = {
+  base,
+};
